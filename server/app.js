f7a21e9e8e5ba9e2a0bd373e45bcbf727731afbb
PhongThai2504 <thaitam25042003@gmail.com>
2025-09-22 01:09:08 +0700

    cap nhat code ngay 21 thang 09 nam 2025

--- server/app.js
+++ server/app.js
@@ -3,12 +3,19 @@ import { connectDb } from './utils/features.js'
 import dotenv from 'dotenv'
 import { errorMiddleware } from './middlewares/error.js'
 import cookieParser from 'cookie-parser'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import { v4 as uuid } from 'uuid'
 
 import userRoute from './routes/user.js'
 import chatRoute from './routes/chat.js'
 import adminRoute from './routes/admin.js'
 import { createUser } from './seeders/user.js'
 import { createGroupChats, createMessages, createMessagesInAChat, createSingleChats } from './seeders/chat.js'
+import { NEW_MESSAGE } from './constants/events.js'
+import { getSockets } from './lib/helper.js'
+
+
 
 
 dotenv.config({
@@ -18,6 +25,7 @@ const mongoURL = process.env.MONGO_URL
 const port = 3000 || process.env.PORT
 const envMode = process.env.NODE_ENV.trim() || 'PRODUCTION'
 const adminSecretKey = process.env.ADMIN_SECRET_KEY || 'koregajiyuda'
+const userSocketIDs = new Map()
 connectDb(mongoURL)
 
 // createUser(10)
@@ -28,6 +36,8 @@ connectDb(mongoURL)
 
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server, {})
 
 //Using middlewares
 app.use(express.json())
@@ -43,14 +53,54 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+io.on('connection', (socket) => {
+    const user = {
+        _id: 'asdsadasd',
+        name: 'Namgon'
+    }
+    userSocketIDs.set(user._id.toString(), socket.id)
+    console.log(userSocketIDs);
+
+    socket.on(NEW_MESSAGE, async ({ chatId, members, message }) => {
+
+        const messageForRealTime = {
+            content: message,
+            _id: uuid(),
+            sender: {
+                _id: user._id,
+                name: user.name
+            },
+            chatId: chatId,
+            createdAt: new Date().toISOString()
+        }
+
+        const messageForDB = {
+            content: message,
+            sender: user._id,
+            chat: chatId
+        }
+
+        const membersSocket = getSockets(members)
+
+        console.log('New Message:', messageForRealTime);
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected');
+        userSocketIDs.delete(user._id.toString())
+        console.log(userSocketIDs);
+    })
+})
+
 app.use(errorMiddleware)
 
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port} in ${envMode} Mode.`)
 })
 
 export {
     envMode,
-    adminSecretKey
+    adminSecretKey,
+    userSocketIDs
 }
